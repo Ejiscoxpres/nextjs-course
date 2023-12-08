b9dc18ce17c2995c39521a0ab46ec531cd1abc7c
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-08 23:05:01 +0100

    dynamic route

--- pages/portfolio/[projectId].js
+++ pages/portfolio/[projectId].js
@@ -1,4 +1,15 @@
+import {useRouter} from 'next/router'
+
 export default function PortfolioProjectPage(){
+
+    const router= useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);
+
+    //send some request to some backend server
+    // to fetch some piece of data with an id of router.query.projectId
+
     return(
         <div>
             <h1>The Portfolio Project Page</h1>
