6d61eb7ed1e4ff73023d9b044f03efae3ca40c7d
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-09 21:39:42 +0100

    navigating to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+
 export default function ClientPage(){
+
+    const clients =[
+        {id:"eji",name:"Ejike"},
+        {id:"ejik",name:"Ejikeme"},
+    ];
     return(
         <div>
             <h1>The Client Page</h1>
+            <ul>
+                <li><Link href="/clients/eji">Ejike</Link></li>
+
+                <li><Link href="/clients/ejik">Ejikeme</Link></li>
+            </ul>
         </div>
     );
 }
