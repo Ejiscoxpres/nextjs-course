e79c27dacf2ff3060392eb29e898ed5f8e63a5d8
Ejiscoexpress <ejiscoexpress@gmail.com>
2023-12-09 22:31:53 +0100

    navigating programatically

--- pages/clients/[Id]/index.js
+++ pages/clients/[Id]/index.js
@@ -1,12 +1,18 @@
 import {useRouter} from 'next/router'
 
 export default function ClientProjectsPage(){
+    function loadProjectHandler(){
+        //load data...
+        router.push('/clients/eji/projecta');
+
+    }
 
     const router=useRouter();
     console.log(router.query);
     return(
         <div>
             <h1>The Project of a given client</h1>
+            <button onClick={loadProjectHandler}>Load Project A</button>
         </div>
     );
 }
